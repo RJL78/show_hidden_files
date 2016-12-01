15ac15c135aecdc6a766e932912928aa16e14d21
Raphael Laporte <rlaporte@Raphaels-MBP-3.wv.cc.cmu.edu>
2016-12-01 17:54:06 -0500

    Set Default to hide hidden files + Error Message if OS not recognized + draft for Windows case

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ define(function (require, exports, module) {
         Menus           = brackets.getModule("command/Menus"),
         Prefs           = brackets.getModule("preferences/PreferencesManager").getExtensionPrefs("show_hidden"),
         MY_COMMAND_ID   = "show_hidden.toggle";
+        OS              = getOS();
 
 
 
@@ -20,12 +21,12 @@ define(function (require, exports, module) {
            // package-style naming to avoid collisions
 
         CommandManager.register("Show Hidden Files", MY_COMMAND_ID, toggle);
-        CommandManager.get(MY_COMMAND_ID).setChecked(1);
+        CommandManager.get(MY_COMMAND_ID).setChecked(0);
 
         var menu = Menus.getMenu(Menus.AppMenuBar.VIEW_MENU);
         menu.addMenuItem(MY_COMMAND_ID);
 
-        Prefs.definePreference("showing_hidden_files", "boolean", true);
+        Prefs.definePreference("showing_hidden_files", "boolean", false);
         Prefs.save();
 
         var FileSystem  = brackets.getModule("filesystem/FileSystem");
@@ -52,55 +53,64 @@ define(function (require, exports, module) {
 
 
     function toggle(){ 
+
+        if (OS.equals("unknown")){
+            window.alert("Operating System Unrecognized"); 
+            return;
+        }
         
        if (Prefs.get("showing_hidden_files")){
             Prefs.set("showing_hidden_files",false); 
             CommandManager.get(MY_COMMAND_ID).setChecked(0);
-            window.alert("SET TO FALSE")
+            
         }
         else {
             Prefs.set("showing_hidden_files",true); 
             CommandManager.get(MY_COMMAND_ID).setChecked(1);
         }
+
         Prefs.save();
         ProjectManager.refreshFileTree();
+        return;
     }
 
     function is_hidden_file(path,name) 
     {
-        return name.match(/^\.[\w]+/);
+        switch (OS) {
+            case 'Windows': 
+            case 'Windows 8.1':
+            case 'Windows 8':
+            case 'Windows 7': 
+            case 'Windows Vista':
+            case 'Windows XP': 
+            case 'Windows 2000': 
+                var fswin = require("fswin"); 
+                // should return JSON object ? We're looking for the IS_HIDDEN attribute
+                fswin.getAttributesSync(path+name);
+                return ???? ; 
+
+            case 'Linux':
+            case 'Mac OS X': 
+                return name.match(/^\.[\w]+/);
+            default: 
+                return false;
+        }
+     
 
     }
 
     function getOS(){
         var os = "unknown";
         var os_list = [
-            {s:'Windows 10', r:/(Windows 10.0|Windows NT 10.0)/},
+            {s:'Windows', r:/(Windows 10.0|Windows NT 10.0)/},
             {s:'Windows 8.1', r:/(Windows 8.1|Windows NT 6.3)/},
             {s:'Windows 8', r:/(Windows 8|Windows NT 6.2)/},
             {s:'Windows 7', r:/(Windows 7|Windows NT 6.1)/},
             {s:'Windows Vista', r:/Windows NT 6.0/},
-            {s:'Windows Server 2003', r:/Windows NT 5.2/},
             {s:'Windows XP', r:/(Windows NT 5.1|Windows XP)/},
             {s:'Windows 2000', r:/(Windows NT 5.0|Windows 2000)/},
-            {s:'Windows ME', r:/(Win 9x 4.90|Windows ME)/},
-            {s:'Windows 98', r:/(Windows 98|Win98)/},
-            {s:'Windows 95', r:/(Windows 95|Win95|Windows_95)/},
-            {s:'Windows NT 4.0', r:/(Windows NT 4.0|WinNT4.0|WinNT|Windows NT)/},
-            {s:'Windows CE', r:/Windows CE/},
-            {s:'Windows 3.11', r:/Win16/},
-            {s:'Android', r:/Android/},
-            {s:'Open BSD', r:/OpenBSD/},
-            {s:'Sun OS', r:/SunOS/},
             {s:'Linux', r:/(Linux|X11)/},
-            {s:'iOS', r:/(iPhone|iPad|iPod)/},
             {s:'Mac OS X', r:/Mac OS X/},
-            {s:'Mac OS', r:/(MacPPC|MacIntel|Mac_PowerPC|Macintosh)/},
-            {s:'QNX', r:/QNX/},
-            {s:'UNIX', r:/UNIX/},
-            {s:'BeOS', r:/BeOS/},
-            {s:'OS/2', r:/OS\/2/},
-            {s:'Search Bot', r:/(nuhk|Googlebot|Yammybot|Openbot|Slurp|MSNBot|Ask Jeeves\/Teoma|ia_archiver)/}
         ];
 
         for (var id in os_list) {
